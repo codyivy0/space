4c6358df3571e578899d0a793c2b0715ebf55901
codyivy0 <codyivy0@gmail.com>
2023-11-09 14:36:33 -0600

    css breakpoints/layout for m/t/d view

---
+++ index.js
@@ -0,0 +1,11 @@
+// when someone clicks the hamburger menu
+// if the menu is closed, open it
+// if the menu is open, close it
+
+const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNavigation = document.querySelector(".primary-navigation");
+
+mobileNavToggle.addEventListener("click", () => {
+  primaryNavigation.classList.toggle("open");
+  mobileNavToggle.classList.toggle("open");
+});
