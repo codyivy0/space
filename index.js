10eea4a75d390dcc56311a2638009a21c77b880a
codyivy0 <codyivy0@gmail.com>
2023-11-10 15:22:21 -0600

    /

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // when someone clicks the hamburger menu
 // if the menu is closed, open it
 // if the menu is open, close it
+// .mobile-nav-toggle.open is not what hes using here
 
 const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNavigation = document.querySelector(".primary-navigation");
